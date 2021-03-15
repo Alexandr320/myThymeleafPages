a3ea644dd81a9f9b2cd9aa07129ad9d86a9b0f33
Alex <a-priadkin.yandex.ru>
2021-03-15 15:02:32 +0300

    Initial commit

---
+++ src/main/java/com/example/myThymeleafPages/MyThymeleafPagesApplication.java
@@ -0,0 +1,13 @@
+package com.example.myThymeleafPages;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyThymeleafPagesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyThymeleafPagesApplication.class, args);
+	}
+
+}
